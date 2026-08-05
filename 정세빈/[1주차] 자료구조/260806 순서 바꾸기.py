a85ef92a3e6803bb2c9f8280dab8c1e39a78144f
sebeenjeong <sebeen0714@sookmyung.ac.kr>
2026-08-06 00:29:35 +0900

    정세빈: 순서 바꾸기 파일 수정

--- 정세빈/[1주차] 자료구조/260806 순서 바꾸기.py
+++ 정세빈/[1주차] 자료구조/260806 순서 바꾸기.py
@@ -1,15 +1,8 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2868
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww17580\viewh11280\viewkind0
-\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural\partightenfactor0
+def solution(num_list, n):
+    answer = []
+    answer = num_list[n:] + num_list[:n]
+    return answer
 
-\f0\fs24 \cf0 def solution(num_list, n):\
-    answer = []\
-    answer = num_list[n:] + num_list[:n]\
-    return answer\
-\
-\
-print(solution([2,1,6],1))\
-print(solution([5,2,1,7,5],3))}
+
+print(solution([2,1,6],1))
+print(solution([5,2,1,7,5],3))
