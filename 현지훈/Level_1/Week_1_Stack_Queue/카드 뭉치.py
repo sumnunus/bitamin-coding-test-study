305fb6868804c680539cc5fc4da0436d479d2465
Scuty <87815827+scuty31@users.noreply.github.com>
2026-08-12 21:54:02 +0900

    Update 카드 뭉치.py

--- 현지훈/Level_1/Week_1_Stack_Queue/카드 뭉치.py
+++ 현지훈/Level_1/Week_1_Stack_Queue/카드 뭉치.py
@@ -27,8 +27,32 @@ def solution(cards1, cards2, goal):
     return answer
 
 
+def solution2(cards1, cards2, goal):
+    answer = 'Yes'
+
+    # 각 list를 queue로 변환
+    cards1_queue = deque(cards1)
+    cards2_queue = deque(cards2)
+
+    # 목표 문장을 만들 때까지
+    for goal_word in goal:
+        # card1에 해당 단어가 있다면
+        if cards1_queue and cards1_queue[0] == goal_word:
+            cards1_queue.popleft()
+        # card2에 해당 단어가 있다면
+        elif cards2_queue and cards2_queue[0] == goal_word:
+            cards2_queue.popleft()
+        # 만들 수 없다면
+        else:
+            answer = 'No'
+            break
+
+    return answer
+
+
 cards1_input = ["i", "water", "drink"]
 cards2_input = ["want", "to"]
 goal_input = ["i", "want", "to", "drink", "water"]
 
 print(solution(cards1_input, cards2_input, goal_input))
+print(solution2(cards1_input, cards2_input, goal_input))
